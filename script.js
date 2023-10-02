37c88ef3bab0f18c1727568ac71af19dab012c9c
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-10-02 09:13:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let I = prompt("Enter a Number Between 1-100");
+
+for(let N=1; N<=I; N++){
+	if(N%3==0 && N%5==0){
+	alert("FizzBuzz");
+}else if(N%3==0){
+	alert("Fizz");
+}else if(N%5==0){
+	alert("Buzz");
+}
+}
